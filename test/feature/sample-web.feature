b338bef7916b7ac0902b6044a142e2d7eaf502de
Marcio Mendes <mmendes.as@gmail.com>
2017-05-09 10:21:59 -0300

    update resources in web/api/soap features

--- test/feature/sample-web.feature
+++ test/feature/sample-web.feature
@@ -2,7 +2,8 @@ Feature: Cucumber test
 
 @simple_web
     Scenario: I want see the Google Page
-        Given user navigates to 'http://www.google.com.br'
+        Given user navigates to 'http://www.google.com.br'        
         When user fills 'GoogleHome-SearchInput' by replacing text with 'led zeppelin wikipedia'
+        And (custom) simple test
         And user clicks on 'GoogleResult-SiteLink'
-        And user waits for 5 seconds
+        #And user waits for 5 seconds
--- test/step-defs/custom-steps.js
+++ test/step-defs/custom-steps.js
@@ -3,15 +3,16 @@ var helperElement = require('walnutjs/src/support/helper/element');
 var custom = function () {
 
     this.Given(/^\(custom\) simple test$/, function () {
+        var deferred = protractor.promise.defer();
         var item = helperElement.getElementFinder('GoogleHome', 'SearchInput');
-
-        item.getText().then(function (text) {
-            helperVars.addVariable('mteste', text);
+        item.getText().then(function getTextSuccess(text) {            
             console.log('Element Text: ', text);
+            deferred.fulfill();
         });
         console.log("custom simple test");
+        return deferred.promise;
     });
-    
+
 }
 
 module.exports = custom;
